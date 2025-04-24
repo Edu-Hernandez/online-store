98460dc9a5af8e7260be6ea7e3fd0996943d26fb
Alan Edu Hernandez <alaneduh231@gmail.com>
2025-04-24 00:48:43 -0500

    actualizacion de code texts 3

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/online-store">
       <Nav user={user} />
       <Routes>
         <Route path="/" element={<Home />} />
