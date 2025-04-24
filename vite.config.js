6222ad3dd7a0f25d1a9b15bdbc6c157439592a85
Alan Edu Hernandez <alaneduh231@gmail.com>
2025-04-24 01:16:04 -0500

    actualizacion de code texts 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/online-store/',
+  base: '/online-store/',
   plugins: [react()],
 })
